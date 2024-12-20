f501b440fd2bc05e36f7951cabc2532d7458cbbf
Jaffar Ashoor <j3ffarm@gmail.com>
2024-12-20 15:59:29 +0300

    Add day02 solution

---
+++ day02/src/part1.rs
@@ -0,0 +1,42 @@
+use std::{error::Error, fs::File, io::Read};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut file = File::open("./data/input.txt")?;
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+
+    let mut reports: Vec<Vec<usize>> = vec![];
+
+    for line in content.lines() {
+        reports.push(
+            line.split(" ")
+                .collect::<Vec<_>>()
+                .into_iter()
+                .map(|str| str.parse().unwrap())
+                .collect(),
+        )
+    }
+
+    let mut safe_reports_count = 0;
+    for report in reports.iter() {
+        let xs: Vec<isize> = report
+            .windows(2)
+            .map(|slice| (slice[1] as isize - slice[0] as isize))
+            .collect();
+
+        let gradual = xs
+            .iter()
+            .map(|x| x.abs())
+            .all(|diff| diff >= 1 && diff <= 3);
+
+        let same_direction =
+            xs.iter().sum::<isize>().abs() == xs.iter().map(|x| x.abs()).sum::<isize>();
+
+        if gradual && same_direction {
+            safe_reports_count += 1;
+        }
+    }
+
+    println!("Safe Reports Count: {safe_reports_count}");
+    Ok(())
+}
